a4e6aaef9518f3d6159c47655ce86856aa9a8389
August Bonds <august.bonds@protonmail.com>
2019-08-27 18:39:41 +0200

    Add check for weekday

--- app/src/main/java/com/augustbonds/momentsofjoy/NotificationWorker.kt
+++ app/src/main/java/com/augustbonds/momentsofjoy/NotificationWorker.kt
@@ -9,14 +9,10 @@ import java.util.*
 class NotificationWorker(context: Context, workerParameters: WorkerParameters) : Worker(context, workerParameters) {
 
     override fun doWork(): Result {
-        val calendar = Calendar.getInstance()
-        calendar.timeInMillis = System.currentTimeMillis()
-        val hour = calendar.get(Calendar.HOUR_OF_DAY)
 
-        Logger.debug("doWork: hour = $hour")
-        //Only allow notifications between 9 and 18
-        if ( hour < 9 || 18 <= hour){
-            Logger.debug("doWork called in the night. ${hour}h")
+        if (!(isDay() && isWeekday()) ){
+            //Only allow notifications during the workday
+            Logger.debug("doWork called in the night.")
             return Result.success()
         }
 
@@ -25,7 +21,24 @@ class NotificationWorker(context: Context, workerParameters: WorkerParameters) :
             // replace previous notification if any
             notify(0, notification)
         }
+
         return Result.success()
+    }
+
+    private fun isDay() : Boolean {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = System.currentTimeMillis()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+
+        return hour in 9..18
+    }
+
+    private fun isWeekday() : Boolean {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = System.currentTimeMillis()
+        val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+        return dayOfWeek in Calendar.MONDAY..Calendar.FRIDAY
     }
 
 
